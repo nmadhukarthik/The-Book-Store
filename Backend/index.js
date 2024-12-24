33bd4a9c093324c667fcd57b66d4ab2ce9512018
Madhu <nmadhu1995@gmail.com>
2024-12-25 01:39:55 +0800

    changed url to local

--- Backend/index.js
+++ Backend/index.js
@@ -1,40 +1,41 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import clc from "cli-color"
-import cors from "cors"
-import { router as bookRoute } from "./route/book.route.js"
-import { router as userRoute } from "./route/user.route.js"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import clc from "cli-color";
+import cors from "cors";
+import { router as bookRoute } from "./route/book.route.js";
+import { router as userRoute } from "./route/user.route.js";
 
-const app = express()
+const app = express();
 
 //global middlewares
-app.use(cors({
-    origin: 'https://book-store-green-xi.vercel.app',
-    // origin: 'https://book-store-frontend-4rvb659q5-madhu-ns-projects.vercel.app', // Frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    credentials: true // If you need to allow cookies or HTTP authentication
-}))
-app.use(express.json())
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        // origin: 'https://book-store-green-xi.vercel.app',
+        methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+        credentials: true, // If you need to allow cookies or HTTP authentication
+    })
+);
+app.use(express.json());
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT || 4000
-const URI = process.env.MongoDbURI
+const port = process.env.PORT || 4000;
+const URI = process.env.MongoDbURI;
 
 //connect to mongoDB
 try {
-    mongoose.connect(URI)
-    console.log(clc.blue.bold.underline("mongodb connected successfully"))
+    mongoose.connect(URI);
+    console.log(clc.blue.bold.underline("mongodb connected successfully"));
 } catch (error) {
-    console.log(clc.red.bold.underline(error))
+    console.log(clc.red.bold.underline(error));
 }
 
 //defining routes
-app.use("/book", bookRoute)
-app.use("/user", userRoute)
-
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
-})
+    console.log(`Server is listening on port ${port}`);
+});
